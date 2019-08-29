77f672b2eeb133d759c34a7eb465e1adedccc6f2
mkdoh3 <mkdoherty3@gmail.com>
2019-08-28 21:32:31 -0500

    flash card post route and confirmatiopn animation

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -7,4 +7,12 @@ class FlashCardsController < ApplicationController
     get '/flash_cards/new' do
         erb :'flash_cards/new'
     end
+
+    post '/flash_cards' do
+        @flash_card = FlashCard.create(params)
+        @flash_card.user = User.first
+        User.first.flash_cards << @flash_card #this will be updated after we build out sign up / login functionality 
+        @message = "Flash Card created!" if @flash_card.id
+        erb :'/flash_cards/new'
+    end
 end
