f345fe0d8f11542066e24d39cf2801381e6cbbe5
mkdoh3 <mkdoherty3@gmail.com>
2019-08-30 09:40:03 -0500

    show and edit routes and views

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -5,14 +5,39 @@ class FlashCardsController < ApplicationController
     end
 
     get '/flash_cards/new' do
+        puts flash
         erb :'flash_cards/new'
     end
 
     post '/flash_cards' do
-        @flash_card = FlashCard.create(params)
-        @flash_card.user = User.first
-        User.first.flash_cards << @flash_card #this will be updated after we build out sign up / login functionality 
-        @message = "Flash Card created!" if @flash_card.id
-        erb :'/flash_cards/new'
+        if params[:english] != "" && params[:translation] != "" 
+            @flash_card = FlashCard.create(params)
+            @flash_card.user = User.first
+             User.first.flash_cards << @flash_card #this will be updated after we build out sign up / login functionality 
+            flash[:success] = "Flash Card Created!" if @flash_card.id
+        else
+            #just messing around - this could be avoided entirely by setting input = required
+            flash[:error] = "Fill out the dang thing!"
+        end
+        redirect '/flash_cards/new'
     end
+
+    get '/flash_cards/:id' do
+        @flash_card = FlashCard.find_by_id(params[:id])
+        @next_flash_card = FlashCard.where("id > ?", @flash_card.id).first
+        erb :'flash_cards/show'
+    end
+
+    get '/flash_cards/:id/edit' do
+        @flash_card = FlashCard.find_by_id(params[:id])
+        erb :'flash_cards/edit'
+    end
+
+    patch '/flash_cards/:id' do
+    end
+
+    delete '/flash_card/:id' do
+    end
+
+
 end
